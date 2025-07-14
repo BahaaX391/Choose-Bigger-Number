9b05fc91d51451137721c02c691358f4a2a5906d
bahaaX391 <bahaabdallah391@gmail.com>
2025-07-14 19:14:36 +0300

    Added Files And Folders

---
+++ dist/main.js
@@ -0,0 +1,55 @@
+"use strict";
+let btns = document.querySelectorAll(".container .randomNums div button");
+let btn1 = document.querySelector(".container .randomNums div .fBtn");
+let btn2 = document.querySelector(".container .randomNums div .sBtn");
+let correctDiv = document.querySelector(".correct");
+let scoreBtn = document.querySelector(".container .score");
+let score = 0;
+function generateRandomNuumber1() {
+    let firstNum = Math.round(Math.random() * 100);
+    btn1.textContent = firstNum.toString();
+    btn1.setAttribute("value", firstNum);
+}
+function generateRandomNuumber2() {
+    let secondNum = Math.round(Math.random() * 100);
+    btn2.textContent = secondNum.toString();
+    btn2.setAttribute("value", secondNum);
+}
+window.onload = generateRandomNuumber2();
+window.onload = generateRandomNuumber1();
+function compare1() {
+    ;
+    if ((parseInt(btn1.getAttribute("value"))) > (parseInt(btn2.getAttribute("value")))) {
+        generateRandomNuumber1();
+        generateRandomNuumber2();
+        score++;
+        scoreBtn.textContent = score;
+        correctDiv.textContent = "Correct";
+
+    }
+    else if (score > 0) {
+        generateRandomNuumber1();
+        generateRandomNuumber2();
+        score--;
+        scoreBtn.textContent = score;
+        correctDiv.textContent = "Incorrect";
+
+    }
+}
+function compare2() {
+    ;
+    if ((parseInt(btn2.getAttribute("value"))) > (parseInt(btn1.getAttribute("value")))) {
+        generateRandomNuumber1();
+        generateRandomNuumber2();
+        score++;
+        scoreBtn.textContent = score;
+    }
+    else if (score > 0) {
+        generateRandomNuumber1();
+        generateRandomNuumber2();
+        score--;
+        scoreBtn.textContent = score;
+    }
+}
+btn1.addEventListener("click", compare1);
+btn2.addEventListener("click", compare2);
